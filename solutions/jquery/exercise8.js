d5f9c432278dee3a466aab03f17a9f543221b84d
Krisztian Balog <krisztian.balog@uis.no>
2017-02-16 17:23:47 +0100

    jQuery Ex #8

--- solutions/jquery/exercise8.js
+++ solutions/jquery/exercise8.js
@@ -4,19 +4,39 @@
 
 $(document).ready(function() {
 
-    // This is for you to complete
+    /* dealing cards */
+    $("form[name='layout_form']").submit(function(e) {
 
-    var sizeCols = 3;
-    var sizeRows = 4;
+        e.preventDefault();  // preventing the form from submission
 
-    for (var row = 0; row < sizeRows; row++) {
-        for (var col = 0; col < sizeCols; col++) {
-            var card = $("<div></div>").addClass("card");
-            if (col == 0) {
-                card.addClass("clearleft");
+        $("#cardboard").empty();
+
+        var layout = $("#layout").val();
+        var dim = layout.split("x");
+        var sizeCols = dim[0];
+        var sizeRows = dim[1];
+
+        var card;
+        for (var row = 0; row < sizeRows; row++) {
+            for (var col = 0; col < sizeCols; col++) {
+                card = $("<div></div>").addClass("card");
+                if (col == 0) {
+                    card.addClass("clearleft");
+                }
+                $("#cardboard").append(card);
             }
-            $("#cardboard").append(card);
         }
-    }
+
+        var cardWidth = card.outerWidth(true);
+        $("#cardboard").width(sizeCols * cardWidth);
+
+        // alternatively, we can return false to prevent the form from submitting
+        //return false;
+    });
+
+    /* clicking on a card */
+    $("#cardboard").on("click", ".card", function () {
+        $(this).fadeTo(1000, 0);
+    });
 
 });
