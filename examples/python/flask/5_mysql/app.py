74a79b0a5b98dc1082e09957e02c4a7a9434f5c0
Krisztian Balog <krisztian.balog@uis.no>
2017-03-24 00:21:08 +0100

    Flask MySQL example

--- examples/python/flask/5_mysql/app.py
+++ examples/python/flask/5_mysql/app.py
@@ -2,7 +2,7 @@
 Flask: Using MySQL
 """
 
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, flash, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
@@ -12,14 +12,14 @@ app.config["DATABASE_USER"] = "root"
 app.config["DATABASE_PASSWORD"] = "root"
 app.config["DATABASE_DB"] = "dat310"
 app.config["DATABASE_HOST"] = "localhost"
-app.config["DEBUG"] = True  # only for development!
-
+app.debug = True  # only for development!
+app.secret_key = 'some_secret'  # needed for flashing
 
 def get_db():
     if not hasattr(g, "_database"):
-        g.db = mysql.connector.connect(host=app.config["DATABASE_HOST"], user=app.config["DATABASE_USER"],
+        g._database = mysql.connector.connect(host=app.config["DATABASE_HOST"], user=app.config["DATABASE_USER"],
                                        password=app.config["DATABASE_PASSWORD"], database=app.config["DATABASE_DB"])
-    return g.db
+    return g._database
 
 
 @app.teardown_appcontext
@@ -49,12 +49,13 @@ def init():
             sql = "INSERT INTO postcodes (postcode, location) VALUES (%s, %s)"
             cur.execute(sql, (k, v))
         db.commit()  # commit
+        flash("Successful initialization")
     except mysql.connector.Error as err:
         return render_template("error.html", msg=err)
     finally:
         cur.close()
 
-    return render_template("success.html", msg="Successful initialization")
+    return redirect(url_for("index"))
 
 
 @app.route("/listall")
@@ -96,13 +97,52 @@ def do_add():
             sql = "INSERT INTO postcodes (postcode, location) VALUES (%s, %s)"
             cur.execute(sql, (postcode, location))
             db.commit()
+            flash("Postcode added")
         except mysql.connector.Error as err:
             return render_template("error.html", msg="Error adding postcode. (Does it exist already?)")
         finally:
             cur.close()
-        return render_template("success.html", msg="Postcode added")
+        return redirect(url_for("add"))
     else:
         return render_template("error.html", msg="Input error")
+
+
+@app.route("/delete/<postcode>")
+def delete(postcode):
+    """Deletes a given postcode."""
+    db = get_db()
+    cur = db.cursor()
+    try:
+        sql = "DELETE FROM postcodes WHERE postcode=%s"
+        cur.execute(sql, (postcode,))  # it's a tuple
+        db.commit()
+        return redirect(url_for("list_all"))
+    except mysql.connector.Error as err:
+        return render_template("error.html", msg="Error deleting data")
+    finally:
+        cur.close()
+
+
+@app.route("/lookup")
+def lookup():
+    """Looks up a given postcode."""
+    location = None
+    postcode = request.args.get("postcode")
+    if postcode:
+        db = get_db()
+        cur = db.cursor()
+        try:
+            sql = "SELECT location FROM postcodes WHERE postcode=%s"
+            cur.execute(sql, (postcode,))  # it's a tuple
+            try:
+                (location,) = cur.fetchone()  # the result of fetchone() is also a tuple!
+            except:
+                pass  # no matching record was found
+        except mysql.connector.Error as err:
+            return render_template("error.html", msg="Error during search")
+        finally:
+            cur.close()
+    return render_template("lookup.html", postcode=postcode, location=location)
 
 
 @app.route("/")
